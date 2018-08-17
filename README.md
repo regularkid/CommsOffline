ea0a9554015a015fd0ec252ae30f73b3c81489e7
Bryan Perfetto <regularkid@gmail.com>
2018-08-16 21:21:19 -0700

    Initial commit of an empty project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CommsOffline
+
+My entry for the js13kgames.com 2018 competition!
---
+++ src/main.js
@@ -0,0 +1,5 @@
+var screenWidth = 320;
+var screenHeight = 240;
+var screenScale = 2.0;
+
+var aw = new Aw(screenWidth, screenHeight, screenScale, []);
